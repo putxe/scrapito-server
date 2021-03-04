04a9d06d74869203c39ba16ba24975d124d42753
putxe <damien.pucheu@hotmail.fr>
2021-03-04 22:24:10 +0100

    test: add handler post tests

--- test/handler.ts
+++ test/handler.ts
@@ -1,23 +1,16 @@
-// import { expect } from 'chai'
-// import { handleRequest } from '../src/handler'
+import { expect } from 'chai'
+import { handlePost } from '../src/handler'
 
-// describe('handler returns response with request method', () => {
-//   const methods = [
-//     'GET',
-//     'HEAD',
-//     'POST',
-//     'PUT',
-//     'DELETE',
-//     'CONNECT',
-//     'OPTIONS',
-//     'TRACE',
-//     'PATCH',
-//   ]
-//   methods.forEach((method) => {
-//     it(method, async () => {
-//       const result = await handleRequest(new Request('/', { method }))
-//       const text = await result.text()
-//       expect(text).to.include(method)
-//     })
-//   })
-// })
+describe('handlePost', () => {
+  it('should return status 500 and body not supported message if body length <= 0', async () => {
+    const result = await handlePost(new Request('/', { body: JSON.stringify([]) }));
+    const text = await result.text();
+    expect(result.status).to.equal(500);
+    expect(text).to.equal('Body not supported');
+  });
+  it('should return status 200 and no text if body length > 0', async () => {
+    const result = await handlePost(new Request('/', { body: JSON.stringify(['yihouuu']) }));
+    expect(result.status).to.equal(200);
+    expect(result.body).to.equal(null);
+  })
+})
